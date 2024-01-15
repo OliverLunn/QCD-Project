1e63a9dc0d2ca08ea539c3aeaa396edafcfb5f5b
unknown <ppyol1@nottingham.ac.uk>
2024-01-15 11:33:16 +0000

    looking at solve_ivo

--- main.py
+++ main.py
@@ -41,18 +41,20 @@ class Kerr_Resonator:
         rho_d = - i*delta*(a@a_dag@a@rho) - i*(K/2)*(a@a_dag@a_dag@a@a@rho) - epsilon*(a@a_dag@rho)
         + epsilon*(a@a@rho)+ i*delta*(a@rho@a_dag@a) + i*(K/2)*(a@rho@a_dag@a_dag@a@a) - epsilon*(a@rho@a_dag)
         + epsilon*(a@rho@a) - (1/2)*(a_dag@a@rho - 2*(a@rho@a_dag) + rho@a_dag@a)
+        
 
-        return rho_d.flatten()
+        return rho_d
     
     def solve(self, rho_0, t_start, t_stop):
 
-        soln = int.solve_ivp(self.rho_dot, [t_start, t_stop], rho_0.flatten())
+        soln = int.solve_ivp(self.rho_dot, [t_start, t_stop], rho_0, vectorized=True)
+        #soln.y.reshape([self.n_states, self.n_states])
         return soln
 
 
 if __name__ == '__main__':
-    kr = Kerr_Resonator(2)
-    rho_0 = np.outer(kr.n_vector(0), kr.n_vector(0))
-    soln = kr.solve(rho_0, 0, 1)
-
+    kr = Kerr_Resonator(3)
+    t=0
+    rho_0 = np.outer(kr.n_vector(0), kr.n_vector(0)).reshape(-1)
+    soln = kr.solve(rho_0, 0, 10)
     print(soln.y)
