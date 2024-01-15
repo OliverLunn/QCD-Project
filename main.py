6f6e7bed6095f7c2474794a21bc374322cdd7eee
unknown <ppyol1@nottingham.ac.uk>
2024-01-15 13:24:05 +0000

    BROKEN!!!!!!

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.integrate as int
+import scipy.integrate as integrate
+from numpy import vectorize
+
 
 class Kerr_Resonator:
-
 
     def __init__(self, n_states):
         
         self.n_states = n_states    
-
 
     def lowering_operator(self):
         n = np.arange(1, self.n_states, 1)
@@ -28,7 +28,7 @@ class Kerr_Resonator:
                 vector[i-(self.n_states)] = 1
         return vector
 
-    def rho_dot(self, t, rho):
+    def rho_dot(self, t, rho, row):
         delta = 0
         epsilon = 0.5
         K = -0.5
@@ -36,25 +36,29 @@ class Kerr_Resonator:
         a = self.lowering_operator()
         a_dag = self.raising_operator()
         i = 1j #imaginary unit
-        rho = rho.reshape(self.n_states, self.n_states)
 
         rho_d = - i*delta*(a@a_dag@a@rho) - i*(K/2)*(a@a_dag@a_dag@a@a@rho) - epsilon*(a@a_dag@rho)
         + epsilon*(a@a@rho)+ i*delta*(a@rho@a_dag@a) + i*(K/2)*(a@rho@a_dag@a_dag@a@a) - epsilon*(a@rho@a_dag)
         + epsilon*(a@rho@a) - (1/2)*(a_dag@a@rho - 2*(a@rho@a_dag) + rho@a_dag@a)
-        
 
-        return rho_d
+        return rho_d[row]
     
     def solve(self, rho_0, t_start, t_stop):
+        rows = self.n_states
+        t_points = np.linspace(t_start, t_stop, 10)
+        soln_matrix = np.zeros((rows, rows, len(t_points)))
 
-        soln = int.solve_ivp(self.rho_dot, [t_start, t_stop], rho_0, vectorized=True)
-        #soln.y.reshape([self.n_states, self.n_states])
-        return soln
+        for r in range(rows):
+                soln = integrate.solve_ivp(self.rho_dot, [t_start, t_stop], rho_0[r,:], t_eval=t_points, args=(r,))
+                print(soln.y.shape)
+                soln_matrix[r,:,:] = soln.y
+
+        return soln_matrix
 
 
 if __name__ == '__main__':
     kr = Kerr_Resonator(3)
-    t=0
-    rho_0 = np.outer(kr.n_vector(0), kr.n_vector(0)).reshape(-1)
-    soln = kr.solve(rho_0, 0, 10)
-    print(soln.y)
+    t_start, t_stop = 0, 1
+    rho_0 = np.outer(kr.n_vector(0), kr.n_vector(0))
+    soln = kr.solve(rho_0, t_start, t_stop)
+    print(soln)
