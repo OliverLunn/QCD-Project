2ad857620d96102c0d933c6f809e97748800ddd6
Harry-Neal <148877711+Harry-Neal@users.noreply.github.com>
2024-01-16 11:04:11 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ import odeintw
 
 class Kerr_Resonator:
 
-    def __init__(self, n_states):
-        
-        self.n_states = n_states    
+    def __init__(self, n_states,delta,epsilon,K):
+
+        self.n_states = n_states   
+        self.delta = delta
+        self.epsilon = epsilon
+        self.K = K
 
     def lowering_operator(self):
         n = np.arange(1, self.n_states, 1,dtype=complex)
@@ -25,9 +28,9 @@ class Kerr_Resonator:
         return vector
 
     def rho_dot(self, t, rho):
-        delta = 0
-        epsilon = 0.5
-        K = -0.5
+        delta = self.delta
+        epsilon = self.epsilon
+        K = self.K
 
         a = self.lowering_operator()
         a_dag = self.raising_operator()
@@ -38,20 +41,24 @@ class Kerr_Resonator:
         + epsilon*np.dot(np.dot(a,rho),a) - (1/2)*(np.dot(a_dag@a,rho) - 2*np.dot(a,rho)@a_dag) + np.dot(rho,a_dag@a)
     
         return rho_d
+
     
-    def solve(self, rho_0, t_start, t_stop):
+    def rho_dot_element(self,t,rho,n,m):
+        delta = self.delta
+        epsilon = self.epsilon
+        K = self.K
+        i=1j
+        return (-i*delta*n - i*(K/2)*n(n-1) + i*delta*m + i*(K/2)*m(m-1) - (1/2)*n - (1/2)*m)*rho[n,m] + epsilon*np.sqrt(n)*rho[n-1,m] + epsilon*np.sqrt(m)*rho[n,m-1] - epsilon*np.sqrt(n+1)*rho[n+1,m] - epsilon*np.sqrt(m+1)*rho[n,m+1] + np.sqrt(n+1)*np.sqrt(m+1)*rho[n+1,m+1]
+    
+    def solve(self,n,m,rho_0):
+        pass
 
-        soln = odeintw.odeintw(self.rho_dot,rho_0,np.linspace(t_start,t_stop,1000))
-        return soln
-
-
+    
 if __name__ == '__main__':
-    kr = Kerr_Resonator(3)
-    t=0
+    delta = 0
+    epsilon = 1
+    K = -0.5
+    n_states = 3
+    kr = Kerr_Resonator(n_states,delta,epsilon,K)
     rho_0 = (np.outer(kr.n_vector(0), kr.n_vector(0)))
-    soln = kr.solve(rho_0, 0, 100)
-    plt.plot(np.real(soln[:,1,1]),'.',color = 'blue')
-    plt.plot(np.real(soln[:,0,0]),'.',color = 'red')
-    plt.plot(np.real(soln[:,0,1]),'.',color = 'green')
-    plt.plot(np.real(soln[:,1,0]),'.',color = 'orange')
-    plt.show()
+
