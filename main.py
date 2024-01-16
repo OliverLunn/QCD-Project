ebcb8d37ffbd1fe632054a6e67d8a48a5118d95b
unknown <ppyol1@nottingham.ac.uk>
2024-01-16 12:23:54 +0000

    matrix rho_dot built

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import odeintw
 
 class Kerr_Resonator:
 
-    def __init__(self, n_states,delta,epsilon,K):
+    def __init__(self, n_states, delta, epsilon, K):
 
         self.n_states = n_states   
         self.delta = delta
@@ -23,8 +23,8 @@ class Kerr_Resonator:
         return operator
 
     def n_vector(self, n):
-        comp = np.arange(0,self.n_states,1)
-        vector = np.where(comp==n,1+0j,0+0j)
+        comp = np.arange(0, self.n_states, 1)
+        vector = np.where(comp==n, 1+0j, 0+0j)
         return vector
 
     def rho_dot(self, t, rho):
@@ -48,16 +48,43 @@ class Kerr_Resonator:
         epsilon = self.epsilon
         K = self.K
         i=1j
-        return (-i*delta*n - i*(K/2)*n(n-1) + i*delta*m + i*(K/2)*m(m-1) - (1/2)*n - (1/2)*m)*rho[n,m] + epsilon*np.sqrt(n)*rho[n-1,m] + epsilon*np.sqrt(m)*rho[n,m-1] - epsilon*np.sqrt(n+1)*rho[n+1,m] - epsilon*np.sqrt(m+1)*rho[n,m+1] + np.sqrt(n+1)*np.sqrt(m+1)*rho[n+1,m+1]
-    
+        rho_d = (-i*delta*n - i*(K/2)*n*(n-1) + i*delta*m + i*(K/2)*m*(m-1) - (1/2)*n - (1/2)*m)*rho[n,m]
+        + epsilon*np.sqrt(n)*rho[n-1,m] + epsilon*np.sqrt(m)*rho[n,m-1] - epsilon*np.sqrt(n+1)*rho[n+1,m]
+        - epsilon*np.sqrt(m+1)*rho[n,m+1] + np.sqrt(n+1)*np.sqrt(m+1)*rho[n+1,m+1]
+
+        return rho_d
+
+    def rho_dot(self, rho):
+
+        delta = self.delta
+        epsilon = self.epsilon
+        K = self.K
+        n_states = self.n_states
+
+        rho_dot = np.zeros((n_states+1, n_states+1))
+        rho = np.pad(rho, 1, mode="constant")
+
+        t=0
+
+        for n in range(n_states+1):
+            for m in range(n_states+1):
+                rho_dot[n,m] = self.rho_dot_element(t, rho, n, m)
+
+        return rho_dot
+
+
     def solve(self,n,m,rho_0):
         pass
 
     
 if __name__ == '__main__':
+
     delta = 0
     epsilon = 1
     K = -0.5
-    n_states = 3
-    kr = Kerr_Resonator(n_states,delta,epsilon,K)
+    n_states = 5
+
+    kr = Kerr_Resonator(n_states, delta, epsilon, K)
     rho_0 = (np.outer(kr.n_vector(0), kr.n_vector(0)))
+    rho_dot = kr.rho_dot(rho_0)
+    print(rho_dot)
